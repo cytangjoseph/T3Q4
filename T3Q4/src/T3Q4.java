91e25653e3099ed171b0d6f32f2369440d46bfcb
cytangjoseph <>
2016-03-10 21:40:26 +0800

    initial version

--- T3Q4/src/T3Q4.java
+++ T3Q4/src/T3Q4.java
@@ -1,9 +1,12 @@
 import java.io.*;
 
 public class T3Q4 {
-    public static void main(String[] args){
+    public static void main(String[] args)throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String pathname;
         
-    
-        System.out.println("hello world");
+        System.out.println("Enter filename: ");
+        pathname = reader.readLine();
+        System.out.println("the filename is " + pathname);
     }
 }
